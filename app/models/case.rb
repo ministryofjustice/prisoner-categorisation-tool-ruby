5ef9bcfc43d3360b7a54c338fc5ec4788e7f0cbc
Himal Mandalia <himalmandalia@gmail.com>
2018-10-01 16:06:09 +0100

    Basic validations to Case model.

--- app/models/case.rb
+++ app/models/case.rb
@@ -1,2 +1,17 @@
 class Case < ApplicationRecord
+  STATES = %w( initial recategorisation advised referred closed pending )
+
+  validates :name, presence: true
+  validates :nomis_id, presence: true
+  validates :due_date, presence: true
+  validates :status, presence: true
+  validates :status, inclusion: { in: STATES }
+
+  before_save :set_due_date
+
+  private
+
+  def set_due_date
+    self.due_date = 10.days.from_now
+  end
 end
