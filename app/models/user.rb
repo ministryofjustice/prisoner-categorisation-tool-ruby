2325b10a902a07fae2609fea4472d04be9823440
Himal Mandalia <himalmandalia@gmail.com>
2018-10-01 16:32:04 +0100

    Add roles and validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  ROLES = %w( offender_manager supervisor security_department regional_intelligence_unit )
+
+  validates :name, presence: true
+  validates :name, uniqueness: { case_sensitive: false }
+  validates :role, presence: true
+  validates :role, inclusion: ROLES
 end
