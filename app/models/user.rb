f6c2f26d05273feaff755ce700dc465855f46382
Himal Mandalia <himalmandalia@gmail.com>
2018-10-03 11:40:09 +0100

    Add convenience methods for roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  ROLES = %w( offender_manager supervisor security_department regional_intelligence_unit )
+  ROLES = %w( offender_manager supervisor security_department regional_intelligence_unit ).freeze
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -7,4 +7,10 @@ class User < ApplicationRecord
 
  validates :role, presence: true
  validates :role, inclusion: ROLES
+
+ ROLES.each do |role|
+   define_method "is_#{role}?" do
+     self.role == role
+   end
+ end
 end
