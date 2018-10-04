8bfbe77620460f080fb7993491aee75b39d258d2
Jonathan Kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2018-10-04 17:02:16 +0100

    Section 5 regurgitates data about case

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,8 @@ class Answer < ApplicationRecord
   belongs_to :case
   belongs_to :section
 
+  default_scope { order(section_id: :asc) }
+
   def display_text
     JSON.parse(self.serialized_answers)
   end
